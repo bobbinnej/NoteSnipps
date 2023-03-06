c6c1e1d735b6b37180af58c1fc98767397b3e52f
jackie <bobbinnej@gmail.com>
2023-03-06 13:21:16 +0300

    mongoose database connection

--- server/index.js
+++ server/index.js
@@ -13,3 +13,12 @@ app.use(express.json());
 app.use(bodyParser.json({limit:"30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
 app.use(cors());
+
+// mongoose setup
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then(()=>{
+    app.listen(PORT, ()=>console.log(`Server is running on port: ${PORT}`))
+}).catch((error)=>console.log(`${error} did not connect`))
