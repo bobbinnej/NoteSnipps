673b76f593a12228fa704317e0e864635fe64c3b
jackie <bobbinnej@gmail.com>
2023-03-15 12:20:39 +0300

    find and fix bug

--- client/src/components/NotesFolder/NotesList.js
+++ client/src/components/NotesFolder/NotesList.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Note from './Note/Note';
 import useStyles from './styles';
+import {useSelector} from 'react-redux';
 
 function NotesList() {
   const classes=useStyles();
+  const noteReducers=useSelector((state)=>state.noteReducers);
+  console.log(noteReducers);
   return (
     <div className={classes.NotesList}>
         <Note/>
--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import {legacy_createStore, applyMiddleware, compose} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
-const store= legacy_createStore(reducers, compose(applyMiddleware(thunk)));
+const store= createStore(reducers, compose(applyMiddleware(thunk)));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
